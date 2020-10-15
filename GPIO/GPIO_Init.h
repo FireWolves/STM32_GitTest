3572091f0fc51082624bb53788a8b25ecf7bb7d5
zhaoth <zhaoth233@163.com>
2020-10-15 23:57:42 +0800

    添加GPIO_Init函数声明

--- GPIO/GPIO_Init.h
+++ GPIO/GPIO_Init.h
@@ -3,5 +3,6 @@
 
 #include "stm32f10x.h"
 #include "stm32f10x_gpio.h"
-
+#include "stm32f10x_conf.h"
+void GPIO_InitConfig(GPIO_InitTypeDef *GPIOx, GPIOMode_TypeDef GPIO_Mode_x, GPIOSpeed_TypeDef GPIO_Speed_x, uint16_t GPIO_Pin_x);
 #endif /* GPIO_Init.h */
--- MAIN/main.h
+++ MAIN/main.h
@@ -2,6 +2,7 @@
 #define __MAIN_H__
 
 #include "stm32f10x.h"
+#include "stm32f10x_conf.h"
 #include "stm32f10x_gpio.h"
 #include "GPIO_Init.h"
 
