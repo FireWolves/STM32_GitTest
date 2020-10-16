e5354b1b65c70a485185970df4d291b38ecdfff1
zhaoth <zhaoth233@163.com>
2020-10-16 14:20:13 +0800

    更改PA10相关配置

--- MAIN/main.c
+++ MAIN/main.c
@@ -5,6 +5,6 @@ u8 i = 0;
 int main(void)
 {
 	__NOP();
-	GPIO_InitConfig(GPIOA,GPIO_Mode_Out_PP,GPIO_Speed_50MHz,GPIO_Pin_10);
+	GPIO_InitConfig(GPIOA,GPIO_Mode_AIN,GPIO_Speed_10MHz,GPIO_Pin_10);
 	
 }
