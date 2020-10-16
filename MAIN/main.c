961f0381f9288382b0d1409f0590024055d2884a
zhaoth <zhaoth233@163.com>
2020-10-16 14:22:09 +0800

    修改为初始化PA10，PA11

--- MAIN/main.c
+++ MAIN/main.c
@@ -5,6 +5,6 @@ u8 i = 0;
 int main(void)
 {
 	__NOP();
-	GPIO_InitConfig(GPIOA,GPIO_Mode_Out_PP,GPIO_Speed_50MHz,GPIO_Pin_10);
+	GPIO_InitConfig(GPIOA,GPIO_Mode_Out_PP,GPIO_Speed_50MHz,GPIO_Pin_10|GPIO_Pin_11);
 	
 }
